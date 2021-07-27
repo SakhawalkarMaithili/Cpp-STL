171f45b57339c71bbbe5f1d636922ff7456a5895
Maithili Sakhawalkar <maithilisatara@gmail.com>
2021-07-27 15:18:57 +0530

    Vector

--- vector_stl.cpp
+++ vector_stl.cpp
@@ -95,7 +95,7 @@ void display (vector <int> &v)
     for (int i = 0; i < v.size(); i++)
     {
         cout << v[i] << " ";
-        cout << v.at(i) << " ";                 // same thing as above but different syntax
+        cout << v.at(i) << " ";                 // same thing as above but different syntax (method)
     }
     cout << endl;
 }
